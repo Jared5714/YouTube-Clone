31bbbc619c2697f9dac0e49290a0fa85c815e4df
Jared Mudd <jared5714@gmail.com>
2018-06-19 22:16:43 +0000

    first commit

---
+++ src/components/video_detail.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const VideoDetail = ({video}) => {
+    
+    if(!video){
+        return <div>Loading...</div>;
+    }
+    const videoId = video.id.videoId;
+    const url = `https://www.youtube.com/embed/${videoId}`;
+    console.log(url);
+    
+
+  return (
+    <div className = "video col-md-8">
+    <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src = {url} allowfullscreen></iframe>
+    </div>
+    <div className = "details">
+    <div><h6>{video.snippet.title}</h6></div>
+    <div><p>{video.snippet.description}</p></div>
+    </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,60 @@
-import React from 'react';
+import React, {Component}  from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import _ from 'lodash';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDmnsb_JA3DAOC-uwfQKaqZ3K-Em_oHs5E';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create new component. Produce some HTML.
+class App extends Component {
+    
+    constructor(props){
+        super(props);
+        
+        this.state = { 
+            videos: [], 
+            selectedVideo: null,
+        };
+        
+        this.videoSearch('batman');
+    }
+    
+    videoSearch(term){
+        
+        YTSearch({key: API_KEY, term: term, maxResults: 50, part: 'snippet'}, videos => {
+            this.setState({ 
+                videos: videos, 
+                selectedVideo: videos[0]
+            });
+            console.log(videos);
+        });
+    }
+    
+    render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+        
+        
+        
+        return (
+        <div>
+            <SearchBar
+            onSearchTermChange = {term => this.videoSearch(term)}/>
+            
+            <VideoDetail video = {this.state.selectedVideo}/>
+            
+            <VideoList 
+            onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+            videos = {this.state.videos} />
+            
+        </div>
+        );
+    }
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+// Take component's HTML and show in DOM.
+
+ReactDOM.render(<App />, document.querySelector('.container'));
